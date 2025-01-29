c2c37baf74a9b7d2f77e7bd0725eed29d8108cb0
alibaba0010 <zakariyahali100@gmail.com>
2025-01-29 11:42:45 +0100

    first commit

---
+++ main.ts
@@ -0,0 +1,24 @@
+import { Hono } from "jsr:@hono/hono";
+const app = new Hono();
+app.use("*", async (c, next) => {
+  c.header("Access-Control-Allow-Origin", "*");
+  c.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  c.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (c.req.method === "OPTIONS") {
+    // return c.text("", 204);
+    return new Response("", { status: 204 });
+  }
+  await next();
+});
+
+app.get("/", (c) => {
+  const response = {
+    email: "adedibu21@gmail.com",
+    timestamp: new Date().toISOString(),
+    githubUrl: "https://github.com/alibaba0010/stage-zero",
+  };
+  return c.json(response);
+});
+Deno.serve(app.fetch);
+
+// CORS Handling
